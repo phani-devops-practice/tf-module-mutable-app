8d74e5daeecf3f109e708b4ca800ed0b9b880920
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-12-03 14:38:34 +0530

    updating ./terraform/tf-module-mutable-app

---
+++ vars.tf
@@ -0,0 +1,25 @@
+// APP
+variable "ENV" {}
+variable "INSTANCE_TYPE" {}
+variable "PRIVATE_SUBNET_IDS" {}
+variable "ALLOW_SG_CIDR" {}
+variable "VPC_ID" {}
+variable "PORT" {}
+variable "WORKSTATION_IP" {}
+variable "COMPONENT" {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
